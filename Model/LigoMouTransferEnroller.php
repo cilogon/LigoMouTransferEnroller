199720ce197d5a03f7002088277238612dea4e04
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-01-07 13:31:31 +0200

    Use co person id instead of the identifier when quering for Roles

--- Model/LigoMouTransferEnroller.php
+++ Model/LigoMouTransferEnroller.php
@@ -67,27 +67,20 @@ class LigoMouTransferEnroller extends AppModel {
    *
    * @since  COmanage Registry v4.1.0
    * @param  Integer      $coId       CO  ID
-   * @param  String       $identifier CO Person Identifier
+   * @param  Integer      $co_person_id CO Person Id
    * @param  Array        $status     List of statuses to filter by
    * @return array|null   list of COUs
    */
 
-  public function getPersonRolesByStatus($coId, $identifier, $status = array()) {
-    $args                                        = array();
-    $args['joins'][0]['table']                   = 'cm_identifiers';
-    $args['joins'][0]['alias']                   = 'Identifier';
-    $args['joins'][0]['type']                    = 'INNER';
-    $args['joins'][0]['conditions'][0]           = 'CoPersonRole.co_person_id=Identifier.co_person_id';
-    $args['conditions']['CoPersonRole.status']   = $status;
-    $args['conditions']['Identifier.identifier'] = $identifier;
-    $args['conditions']['Identifier.status']     = StatusEnum::Active;
-    $args['conditions']['Cou.co_id']             = $coId;
-    $args['conditions'][]                        = 'CoPersonRole.cou_id IS NOT null';
-    $args['conditions'][]                        = 'CoPersonRole.co_person_role_id IS null';
-    $args['conditions'][]                        = 'CoPersonRole.deleted IS NOT TRUE';
-    $args['conditions'][]                        = 'Identifier.identifier_id IS NULL';
-    $args['conditions'][]                        = 'Identifier.deleted IS NOT TRUE';
-    $args['fields']                              = array(
+  public function getPersonRolesByStatus($coId, $co_person_id, $status = array()) {
+    $args                                              = array();
+    $args['conditions']['CoPersonRole.status']         = $status;
+    $args['conditions']['CoPersonRole.co_person_id']   = $co_person_id;
+    $args['conditions']['Cou.co_id']                   = $coId;
+    $args['conditions'][]                              = 'CoPersonRole.cou_id IS NOT null';
+    $args['conditions'][]                              = 'CoPersonRole.co_person_role_id IS null';
+    $args['conditions'][]                              = 'CoPersonRole.deleted IS NOT TRUE';
+    $args['fields']                                    = array(
       'DISTINCT CoPersonRole.id',
       'CoPersonRole.title',
       'CoPersonRole.affiliation',
@@ -123,11 +116,10 @@ class LigoMouTransferEnroller extends AppModel {
    * @since  COmanage Registry v4.1.0
    * @param  Integer      $coId          CO  ID
    * @param  Integer      $coPetitionId  CO Petition  ID
-   * @param  String       $identifier    CO Person Identifier
    * @return array|null   CoPersonRole
    */
 
-  public function getCoPersonRoleFromPetition($coId, $coPetitionId, $identifier) {
+  public function getCoPersonRoleFromPetition($coId, $coPetitionId) {
     $args                                        = array();
     $args['joins'][0]['table']                   = 'cm_co_petitions';
     $args['joins'][0]['alias']                   = 'CoPetition';
@@ -135,7 +127,6 @@ class LigoMouTransferEnroller extends AppModel {
     $args['joins'][0]['conditions'][0]           = 'CoPetition.cou_id=Cou.id';
     $args['joins'][0]['conditions'][1]           = 'CoPetition.id=' . $coPetitionId;
     $args['conditions'][]                        = "CoPetition.enrollee_co_person_id=CoPersonRole.co_person_id";
-    $args['conditions'][]                        = "CoPersonRole.actor_identifier='{$identifier}'";
     $args['conditions'][]                        = 'CoPersonRole.co_person_role_id IS NULL';
     $args['conditions'][]                        = 'CoPersonRole.deleted IS NOT TRUE';
 
